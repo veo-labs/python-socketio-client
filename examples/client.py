8b07846d56e470cab71f0df14fb5795add005674
Alexis Métaireau <alexis@notmyidea.org>
2016-02-25 11:58:22 +0100

    Conform to the PEP8 python standard.

--- examples/client.py
+++ examples/client.py
@@ -10,10 +10,12 @@ logging.basicConfig(level=logging.DEBUG)
 io = Manager('localhost', 8000, auto_connect=False)
 chat = io.socket('/chat')
 
+
 @chat.on('welcome')
 def on_hello(*args, **kwargs):
     print args
     kwargs['callback']("thanks!")
+
 
 @chat.on_connect()
 def on_connect():
--- examples/server.py
+++ examples/server.py
@@ -1,18 +1,21 @@
 import socketio
 from gevent import pywsgi
-import six
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 io = socketio.Server(binary=True)
 
+
 def callback(*args):
-    print "Client responded '%s' to welcome" % args
+    print("Client responded '%s' to welcome" % args)
+
 
 @io.on('connect', namespace='/chat')
 def connect(sid, *args):
-    io.emit(u'welcome', {'some':'data'}, room=sid, callback=callback, namespace='/chat')
+    io.emit(u'welcome', {'some': 'data'}, room=sid, callback=callback,
+            namespace='/chat')
+
 
 @io.on('hello', namespace='/chat')
 def message(sid, data):
